e03f2125bd47f9590c7c8e4a32fd07c9c2f243b1
Ryandunn399 <ryandunn399@gmail.com>
2022-05-03 13:27:10 -0400

    v1.2: Steinhoff Update

--- StrongholdAITiming.py
+++ StrongholdAITiming.py
@@ -23,8 +23,12 @@ WOLF_INTERVAL = 35
 PIG_INTERVAL = 88
 SECONDS_TO_HOUR = 3600
 HOURS_TO_DAY = 24
+EST_DIFF = 5
+
+print("Version 1.2: Steinhoff Update")
 
 now = datetime.datetime.now()
+now = now + datetime.timedelta(hours=now.hour + EST_DIFF)
 
 """
 This code will calculate the time until the next wolf attack
@@ -35,8 +39,8 @@ difference_wolf = now - initial_wolf_date
 difference_wolf_hours = difference_wolf.days * HOURS_TO_DAY + difference_wolf.seconds // SECONDS_TO_HOUR
 wolf_ratio = difference_wolf_hours / WOLF_INTERVAL + 1
 
-updated_wolf_date = initial_wolf_date + datetime.timedelta(hours=(WOLF_INTERVAL * math.floor(wolf_ratio)))
-updated_wolf_date_string = updated_wolf_date.strftime("%m/%d/%y @ %I:%M %p")
+updated_wolf_date = initial_wolf_date + datetime.timedelta(hours=(WOLF_INTERVAL * math.floor(wolf_ratio)) - EST_DIFF)
+updated_wolf_date_string = updated_wolf_date.strftime("%m/%d/%y @ %I:%M %p EST")
 
 print("The next wolf attack will be at: ", updated_wolf_date_string)
 
@@ -49,8 +53,7 @@ difference_pig = now - initial_pig_date
 difference_pig_hours = difference_pig.days * HOURS_TO_DAY + difference_pig.seconds // SECONDS_TO_HOUR
 pig_ratio = difference_pig_hours / PIG_INTERVAL + 1
 
-updated_pig_date = initial_pig_date + datetime.timedelta(hours=(PIG_INTERVAL * math.floor(pig_ratio)))
-updated_pig_data_string = updated_pig_date.strftime("%m/%d/%y @ %I:%M %p")
+updated_pig_date = initial_pig_date + datetime.timedelta(hours=(PIG_INTERVAL * math.floor(pig_ratio)) - EST_DIFF)
+updated_pig_data_string = updated_pig_date.strftime("%m/%d/%y @ %I:%M %p EST")
 
 print("The next pig attack will be at: ", updated_pig_data_string)
-print("Reminder to not get tburged")
