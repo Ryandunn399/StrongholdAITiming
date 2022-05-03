07d7cf0abc32fa5ea0bbf7a707e9d743c7757028
Ryandunn399 <ryandunn399@gmail.com>
2022-05-02 22:56:14 -0400

    No magic numbers here, no sir

--- StrongholdAITiming.py
+++ StrongholdAITiming.py
@@ -6,6 +6,10 @@ import datetime
 import math
 from datetime import timedelta
 
+# Program constants
+WOLF_INTERVAL = 35
+PIG_INTERVAL = 88
+
 now = datetime.datetime.now()
 
 """
@@ -17,7 +21,7 @@ difference_wolf = now - initial_wolf_date
 difference_wolf_hours = difference_wolf.days * 24 + difference_wolf.seconds // 3600
 wolf_ratio = difference_wolf_hours / 35 + 1
 
-updated_wolf_date = initial_wolf_date + datetime.timedelta(hours=(35 * math.floor(wolf_ratio)))
+updated_wolf_date = initial_wolf_date + datetime.timedelta(hours=(WOLF_INTERVAL * math.floor(wolf_ratio)))
 updated_wolf_date_string = updated_wolf_date.strftime("%m/%d/%y @ %I:%M %p")
 
 print("The next wolf attack will be at: ", updated_wolf_date_string)
@@ -31,7 +35,7 @@ difference_pig = now - initial_pig_date
 difference_pig_hours = difference_pig.days * 24 + difference_pig.seconds // 3600
 pig_ratio = difference_pig_hours / 88 + 1
 
-updated_pig_date = initial_pig_date + datetime.timedelta(hours=(88 * math.floor(pig_ratio)))
+updated_pig_date = initial_pig_date + datetime.timedelta(hours=(PIG_INTERVAL * math.floor(pig_ratio)))
 updated_pig_data_string = updated_pig_date.strftime("%m/%d/%y @ %I:%M %p")
 
 print("The next pig attack will be at: ", updated_pig_data_string)
